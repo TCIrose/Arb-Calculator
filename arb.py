da5437fcc6502e4aa341d63bec9c6e81b9db7ff3
Tony <57923336+TCIrose@users.noreply.github.com>
2020-06-25 03:16:32 +0300

    Added outcomes calculation. Replaced outcome entries with labels and put all the calculation in one function

--- arb.py
+++ arb.py
@@ -38,11 +38,8 @@ class Arb:
         two.pack()
         thirdentry = tk.Entry(self.master, textvariable = self.odds3)
         thirdentry.pack()
-        savebutton = tk.Button(self.master, text = "save", command = self.Calc3way)
-        savebutton.pack()
-
-        #Adding the stake entries and  labels 
-
+        
+        #Adding the stake entry and  labels 
         #stakes variables
         self.staketotal = tk.StringVar()
         
@@ -72,37 +69,30 @@ class Arb:
         two.pack()
         self.astake = tk.Label(self.master, text = 0.00)
         self.astake.pack()
-        
-        #button to get stakes
-        stakesButton = tk.Button(self.master, text = "calculate", command = self.Calc3wayStakes)
-        stakesButton.pack()
 
-        #Outcomes entries(disabled)
-        self.o1 =tk.StringVar()
-        self.ox =tk.StringVar()
-        self.o2 =tk.StringVar()
-
+        #Outcomes labels
         outcomesLabel = tk.Label(self.master, text = "Outcomes")
         outcomesLabel.pack()
 
         one = tk.Label(self.master, text = "1")
         one.pack()
-        self.outcome1 = tk.Entry(self.master, textvariable = self.o1)
+        self.outcome1 = tk.Label(self.master, text = 0.00)
         self.outcome1.pack()
 
         draw = tk.Label(self.master, text = "X")
         draw.pack()
-        self.outcomex = tk.Entry(self.master, textvariable = self.ox)
+        self.outcomex = tk.Label(self.master, text = 0.00)
         self.outcomex.pack()
         
         two = tk.Label(self.master, text = "2")
         two.pack()
-        self.outcome2 = tk.Entry(self.master, textvariable = self.o2)
+        self.outcome2 = tk.Label(self.master, text = 0.00)
         self.outcome2.pack()
 
         #button to get outcomes
-        outcomesButton = tk.Button(self.master, text = "calculate", command = self.getOutcomes)
-        outcomesButton.pack()
+        savebutton = tk.Button(self.master, text = "save", command = self.Calc3way)
+        savebutton.pack()
+
 
         #profit entry(disabled)
         self.prof = tk.StringVar()
@@ -116,29 +106,12 @@ class Arb:
 
     def Calc3way(self):
         """
-        Method to calculate the percentage of a 3 way arb
+        Method to: 
+        1. Calculate the percentage of a 3 way arb
+        2. Calculate stake to be placed for each outcome
+    
         """
-        o1 = float(self.odds1.get())
-        o2 = float(self.odds2.get())
-        o3 = float(self.odds3.get())
         
-        #Individual Arbitrage calculation
-        o1A = (1/o1)*100
-        o1X = (1/o2)*100
-        o1B = (1/o3)*100
-        totalArb = round((o1A + o1X + o1B),2)
-        
-        #profit entry(disabled)
-        profitEntry = tk.Label(self.master, text = totalArb)
-        profitEntry.pack()
-        print(totalArb)
-        
-
-
-    def Calc3wayStakes(self):
-        """
-        Method to calculate stake to be placed for each outcome
-        """
         #Just the total stake input for now
         o1 = float(self.odds1.get())
         o2 = float(self.odds2.get())
@@ -148,8 +121,16 @@ class Arb:
         o1A = (1/o1)*100
         o1X = (1/o2)*100
         o1B = (1/o3)*100
-        totalArb = round((o1A + o1X + o1B),2)
 
+        #total arbitrage
+        totalArb = round((o1A + o1X + o1B),2)
+        arbperc = round((100 - totalArb),2)
+
+        #profit label
+        profitArb = tk.Label(self.master, text = f"{arbperc}%")
+        profitArb.pack()
+        print(arbperc)
+        
         #get total stake
         Tstake = float(self.staketotal.get())
         
@@ -158,22 +139,21 @@ class Arb:
         stakeX = round(((Tstake*o1X)/totalArb),2)
         stakeB = round(((Tstake*o1B)/totalArb),2)
 
+        #Calculate each possible outcome
+        outcA = round((stakeA*o1), 2)
+        outcX = round((stakeX*o2),2)
+        outcB = round((stakeB*o3),2)
+
         #configure labels to show stakes
         self.hstake.configure(text = stakeA)
-
-        self.dstake.configure(text = stakeX)
-        
+        self.dstake.configure(text = stakeX)        
         self.astake.configure(text = stakeB)
 
-        print(f"{stakeA} {stakeX} {stakeB}")
-        print(stakeA+stakeB+stakeX)
+        #configure labels to show potential outcomes
+        self.outcome1.configure(text = outcA)
+        self.outcomex.configure(text = outcX)
+        self.outcome2.configure(text = outcB)
         
-
-    def getOutcomes(self):
-        ot1 = float(self.o1.get())
-        otx = float(self.ox.get())
-        ot2 = float(self.o2.get())
-        print(f"{ot1} {otx} {ot2}")
 
     def getProfit(self):
         p = float(self.prof.get())
